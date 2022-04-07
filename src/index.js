81ab74c0985ce8b2918e6a11884cc358a6531518
pro100 <pro100.sto@yandex.ru>
2022-04-07 14:20:40 +0300

    create

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// JS
+// import "./js/";
+// import _ from "lodash";
+
+// SCSS
+// import "./assets/scss/main.scss";
+import "./blocks/pages/index.scss";
+// import "./blocks/pages/index.pug";
+// import "./blocks/pages/about.pug";
+// import "../src/index.pug";
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// Vue.js
+// window.Vue = require('vue')
+
+// Vue components (for use in html)
+// Vue.component('example-component', require('./components/Example.vue').default)
+
+// Vue init
+// const app = new Vue({
+//   el: '#app'
+// })
